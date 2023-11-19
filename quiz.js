15fe10e0bb1e92af756244ce351e96f3f0f32c50
bilgee <bilgee1324@gmail.com>
2023-11-20 02:37:20 +0800

    push

---
+++ quiz.js
@@ -0,0 +1,523 @@
+const menus = [
+    {
+        name: 'Movie',
+        image: './images/movie.jpg',
+        type: 'sub',
+        sub: [
+            {
+                name: 'Guess the Movie',
+                type: 'none',
+                quizs: [
+                    {
+                        question: "Which movie features a character named 'Forrest Gump'?",
+                        answers: [ 'Goodfellas', 'Forrest Gump', 'The Matrix', 'Jurassic Park'],
+                        correctAnswer: 'Forrest Gump'
+                    }
+                ]
+            },
+            {
+                name: 'Actor/Actress Trivia',
+                type: 'none'
+            },
+            {
+                name: 'Movie Quotes',
+                type: 'none'
+            },
+            {
+                name: 'Director Identification',
+                type: 'none'
+            },
+            {
+                name: 'Movie Plot Twists',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Sport',
+        image: './images/sport.jpg',
+        type: 'sub',
+        sub: [
+            {
+                name: 'Athlete Trivia',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Team Logos',
+                type: 'none'
+            },
+            {
+                name: 'Sports History',
+                type: 'none'
+            },
+            {
+                name: 'Famous Sports Moments',
+                type: 'none'
+            },
+            {
+                name: 'Sports Rules and Regulations',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Dogs',
+        image: './images/dog.jpg',
+        type: 'sub',
+        sub: [
+            {
+                name: 'Dog Breeds Identification',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Famous Dogs in Pop Culture',
+                type: 'none'
+            },
+            {
+                name: 'Canine Trivia',
+                type: 'none'
+            },
+            {
+                name: 'Dog Health and Care',
+                type: 'none'
+            },
+            {
+                name: 'Doggy History',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Music',
+        type: 'sub',
+        image: './images/music.jpg',
+        sub: [
+            {
+                name: 'Song Title from Lyrics',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Musician Trivia',
+                type: 'none'
+            },
+            {
+                name: 'Album Cover Identification',
+                type: 'none'
+            },
+            {
+                name: 'Music Genres',
+                type: 'none'
+            },
+            {
+                name: 'Music Awards',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Anime',
+        type: 'sub',
+        image: './images/anime.jpg',
+        sub: [
+            {
+                name: 'Guess the Movie',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Actor/Actress Trivia',
+                type: 'none'
+            },
+            {
+                name: 'Movie Quotes',
+                type: 'none'
+            },
+            {
+                name: 'Director Identification',
+                type: 'none'
+            },
+            {
+                name: 'Movie Plot Twists',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Gaming',
+        type: 'sub',
+        image: './images/gaming.jpg',
+        sub: [
+            {
+                name: 'Game Characters',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Gaming History',
+                type: 'none'
+            },
+            {
+                name: 'Game Titles',
+                type: 'none'
+            },
+            {
+                name: 'Gaming Platforms',
+                type: 'none'
+            },
+            {
+                name: 'Gaming Genres',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Cartoon',
+        type: 'sub',
+        image: './images/cartoon.jpg',
+        sub: [
+            {
+                name: 'Guess the Character',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Cartoon Theme Songs',
+                type: 'none'
+            },
+            {
+                name: 'Voice Actor Challenge',
+                type: 'none'
+            },
+            {
+                name: 'Cartoon Trivia',
+                type: 'none'
+            },
+            {
+                name: 'Cartoon Crossovers',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Art',
+        type: 'sub',
+        image: './images/artist.jpg',
+        sub: [
+            {
+                name: 'Guess the Movie',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Actor/Actress Trivia',
+                type: 'none'
+            },
+            {
+                name: 'Movie Quotes',
+                type: 'none'
+            },
+            {
+                name: 'Director Identification',
+                type: 'none'
+            },
+            {
+                name: 'Movie Plot Twists',
+                type: 'none'
+            }
+        ]
+    },
+    {
+        name: 'Code',
+        type: 'sub',
+        image: './images/code.jpg',
+        sub: [
+            {
+                name: 'Language Syntax',
+                type: 'none',
+                quizs: [
+                    {
+                        quiz: 'asdfasdf ?',
+                        answers: [
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                            {
+                                answer: 'kdafjh'
+                            },
+                        ],
+                        correctAnswer: 'hlgdjfjg'
+                    }
+                ]
+            },
+            {
+                name: 'Programming Concepts',
+                type: 'none'
+            },
+            {
+                name: 'Famous Algorithms',
+                type: 'none'
+            },
+            {
+                name: 'Developer Tools and IDEs',
+                type: 'none'
+            },
+            {
+                name: 'Programming History',
+                type: 'none'
+            }
+        ]
+    },
+]
+
+var screenType = 'main';
+
+window.onload = () => {
+    loadMenus(menus);
+}
+
+function loadMenus(menus) {
+
+    const container = document.getElementById('menu-container');
+    container.style.display = 'grid';
+
+    container.innerHTML = '';
+
+    menus.forEach((menu, index) => {
+
+        const div = document.createElement('div');
+        const title = document.createElement('div');
+
+        if(screenType == 'main') {
+            const image = document.createElement('img');
+            image.src = menu.image;
+            image.className = 'menu-image';
+            div.append(image);
+        }
+
+        div.id = 'menu-item-' + (index);
+        div.className = 'menu-item';
+        title.className = 'menu-title';
+        title.innerHTML = menu.name;
+        div.append(title);
+        div.addEventListener('click', onCategoryClick);
+        div.param = menu;
+
+        container.append(div);
+    });
+
+    if(screenType == 'sub') {
+        const backButton = document.createElement('div');
+        const title = document.createElement('div');
+
+        backButton.id = 'menu-item-5';
+        title.className = 'menu-title';
+        title.innerHTML = 'Буцах';
+        backButton.className = 'menu-item';
+        backButton.append(title);
+        backButton.addEventListener('click', backToMain);
+        container.append(backButton);
+    }
+}
+
+function backToMain() {
+    screenType = 'main';
+    loadMenus(menus);
+}
+
+function loadQuiz(item) {
+    const container = document.getElementById('menu-container');
+    container.style.display = 'flex';
+
+    container.innerHTML = '';
+
+    const quiz = item[0];
+
+    const question = document.createElement('div');
+    question.className = 'question';
+    question.innerHTML = quiz.question;
+
+    const answers = document.createElement('div');
+    answers.className = 'answers';
+
+    quiz.answers.forEach((value, index) => {
+        const answer = document.createElement('div');
+        const button = document.createElement('button');
+        answer.classList.add('answer', 'answer-' + index);
+        button.className = 'btn-13'
+        button.innerHTML = value;
+        button.addEventListener('click', chooseAnswer);
+        button.param = {
+            correctAnswer: quiz.correctAnswer,
+            clicked: value
+        };
+        
+        answer.append(button);
+        answers.append(answer);
+    })
+
+    container.append(question);
+    container.append(answers);
+}
+
+function chooseAnswer (event) {
+    const answer = event.currentTarget.param;
+
+    if(answer.clicked == answer.correctAnswer) {
+        this.classList.add('correct-answer');
+    } else {
+        this.classList.add('wrong-answer');
+    }
+}
+
+function onCategoryClick (event) {
+    const clickedMenu = event.currentTarget.param;
+
+    screenType = clickedMenu.type;
+    if(clickedMenu.type == 'sub') {
+        loadMenus(clickedMenu.sub);
+    } else if(clickedMenu.type == 'none') {
+        loadQuiz(clickedMenu.quizs);
+    }
+}
